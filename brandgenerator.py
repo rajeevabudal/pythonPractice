9007fa0ab7d244a4779fa9ac3554cc49658af56c
rajeeva <rajeevabp92@gmail.com>
2023-04-16 10:09:47 +0530

    brand name generator

---
+++ brandgenerator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Brand Name Generator")
+
+city = input("What is the name of the city you grew up = ");
+petName = input("What is your pet name = ");
+
+brandName = city + " " + petName;
+
+print(brandName);
