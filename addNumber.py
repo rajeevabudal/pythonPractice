9e81fe276b8c1b95b1342cfe5df947b0378d26ce
rajeeva <rajeevabp92@gmail.com>
2023-06-21 20:29:34 +0530

    mathematical calc

---
+++ addNumber.py
@@ -0,0 +1,12 @@
+val = input("Enter the input?")
+firstInput = int(val[0])
+secondInput = int(val[1]);
+# addtion
+print(firstInput + secondInput);
+
+#subtration
+print(firstInput - secondInput);
+#divsion
+print(firstInput / secondInput);
+#multiplication
+print(firstInput * secondInput);
