e3446c8c5009df9deca6b9252781077ba2c72c11
rajeeva <rajeevabp92@gmail.com>
2023-06-24 20:51:26 +0530

    calculate the bmi

---
+++ bmi.py
@@ -0,0 +1,10 @@
+weight = input("Enter your weight in kGS: ");
+height = input("Enter your height in M: ");
+
+bmi = int(weight)/float(height) ** 2;
+
+print(bmi);
+
+bmiAsInteger = int(bmi);
+
+print(bmiAsInteger);
