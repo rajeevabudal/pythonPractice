625073e79d7c236a3b87e4cd70a201a0e0db1a1d
rajeeva <rajeevabp92@gmail.com>
2024-02-29 16:45:42 +0530

    learning the pandaas

--- trypanda.py
+++ trypanda.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 data = pd.read_xlsx("/Users/rajeevabudal/Downloads/2318083-car_data.xlsx")
 
-# print(data.head())
-# print(data[["name", "sex"]].head())
-# print(data[data.name == "na"].head())
-# # print(data.tail(1))
-# print(data.shape[0])
+print(data.head())
+print(data[["name", "sex"]].head())
+print(data[data.name == "na"].head())
+# print(data.tail(1))
+print(data.shape[0])
 
-# print(list(data.columns))
+print(list(data.columns))
 
-# print(type(data))
+print(type(data))
 
-# print(data[(data["name"] == "Max Ruin ") & (data["mark"] == 85)].head())
+print(data[(data["name"] == "Max Ruin ") & (data["mark"] == 85)].head())
 
-# print(data[data["mark"].isin ([60, 75])]).head(5)
+print(data[data["mark"].isin ([60, 75])]).head(5)
 
 print(data.dtypes)
